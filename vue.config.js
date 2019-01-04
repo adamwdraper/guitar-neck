601462b6242b4d6601940869865f6bc4762602d5
Adam Draper <adamwdraper@gmail.com>
2019-01-03 23:41:23 -0700

    fix icons

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,7 @@ module.exports = {
     : '/',
   css: {
     loaderOptions: {
-      // pass options to sass-loader
       sass: {
-        // @/ is an alias to src/
-        // so this assumes you have a file named `src/variables.scss`
         data: `@import "@/scss/base.scss";`
       }
     }
