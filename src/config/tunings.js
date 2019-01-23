dc1c30ed95acc83ba7985487cea2c61de99591a1
Adam Draper <adamwdraper@gmail.com>
2019-01-22 21:27:22 -0800

    Update tunings.js

--- src/config/tunings.js
+++ src/config/tunings.js
@@ -24,5 +24,77 @@ export default [
       notes[0],
       notes[5]
     ]
+  },
+  {
+    id: 'double-drop-d',
+    name: 'Double Drop D',
+    notes: [
+      notes[5],
+      notes[2],
+      notes[10],
+      notes[5],
+      notes[0],
+      notes[5]
+    ]
+  },
+  {
+    id: 'd-modal',
+    name: 'D Modal',
+    notes: [
+      notes[5],
+      notes[0],
+      notes[10],
+      notes[5],
+      notes[0],
+      notes[5]
+    ]
+  },
+  {
+    id: 'drop-c-sharp',
+    name: 'Drop C#',
+    notes: [
+      notes[6],
+      notes[1],
+      notes[9],
+      notes[4],
+      notes[11],
+      notes[4]
+    ]
+  },
+  {
+    id: 'drop-c',
+    name: 'Drop C',
+    notes: [
+      notes[5],
+      notes[0],
+      notes[8],
+      notes[3],
+      notes[10],
+      notes[3]
+    ]
+  },
+  {
+    id: 'drop-b',
+    name: 'Drop B',
+    notes: [
+      notes[4],
+      notes[11],
+      notes[7],
+      notes[2],
+      notes[9],
+      notes[2]
+    ]
+  },
+  {
+    id: 'drop-a',
+    name: 'Drop A',
+    notes: [
+      notes[2],
+      notes[9],
+      notes[5],
+      notes[0],
+      notes[7],
+      notes[0]
+    ]
   }
 ];
